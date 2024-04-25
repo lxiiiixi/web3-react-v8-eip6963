1bda1bf2bf3930c7efc3e923c0b183149e40c170
lxiiiixi <104674636+lxiiiixi@users.noreply.github.com>
2024-04-25 09:07:46 +0800

    Feat: add blockNumber

---
+++ src/sections/BlockNumber/index.tsx
@@ -0,0 +1,49 @@
+import { useWeb3React } from "@web3-react/core";
+import { useEffect, useState } from "react";
+
+const useBlockNumber = () => {
+  const [blockNumber, setBlockNumber] = useState(0);
+  const { provider } = useWeb3React();
+
+  useEffect(() => {
+    if (!provider) return;
+    provider
+      .getBlockNumber()
+      .then((res) => {
+        setBlockNumber(res);
+      })
+      .catch((error) => console.error(`Failed to get block number`, error));
+    provider.on("block", (blockNumber) => {
+      // console.log("New Block:", blockNumber);
+      setBlockNumber(blockNumber);
+    });
+  }, [provider]);
+  return blockNumber;
+};
+
+export default function Index() {
+  const blockNumber = useBlockNumber();
+
+  const [isMounted, setIsMounted] = useState(true);
+
+  useEffect(
+    () => {
+      const timer1 = setTimeout(() => setIsMounted(true), 1000);
+
+      // this will clear Timeout when component unmount like in willComponentUnmount
+      return () => {
+        setIsMounted(false);
+        clearTimeout(timer1);
+      };
+    },
+    [blockNumber], //useEffect will run only one time
+    //if you pass a value to array, like this [data] than clearTimeout will run every time this value changes (useEffect re-run)
+  );
+
+  return (
+    <div className="flex-center fixed bottom-0 right-10 my-10">
+      <span style={{ opacity: isMounted ? "0.2" : "0.6" }}>{blockNumber}</span>
+      <div className="ml-2 h-2 w-2 rounded-full bg-green-600"></div>
+    </div>
+  );
+}
--- src/sections/PageLayout.tsx
+++ src/sections/PageLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Network from "./Network";
+import BlockNumber from "./BlockNumber";
 
 function PageLayout() {
   return (
     <div className="h-full w-full text-center">
       <h2 className="my-10 text-3xl">Choose an Injected Wallet To Connect</h2>
+      <BlockNumber />
       <Network />
       <div className="flex-center">
         <Outlet />
